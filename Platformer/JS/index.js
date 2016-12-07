d8c2cfb296b74887bd18c433c09508bf0e1ee1c1
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-07 16:46:48 +0000

    commit

--- Platformer/JS/index.js
+++ Platformer/JS/index.js
@@ -13,27 +13,33 @@ var charictar, ground,
 	charictar = {
 		width:	10,
 		height:	10,
-		x:	scrWidth /6,
+		x:	scrWidth /10,
 		y:	scrHeight /2 -10 , //10 = charictar.height
-		color:	'rgb(255,225,25)',
+		fillColor:	'rgb(255,225,25)',
 		speed:	6,
 		jumpHeight:	6,
 		vX:	0,
 		vY:	0
 	};
 
-	blockType[0].x = scrWidth/4 - 50;
-	blockType[0].y = scrHeight/2 - 15;
+	blockType[0].x = scrWidth/6 -blockType[0].width;
+	blockType[0].y = scrHeight/2 - blockType[0].height;
 
-	blockType[1].x = scrWidth/4 * 2 -50
-	blockType[1].y = scrHeight/2 - 15;
+	blockType[1].x = scrWidth/6 + 60  -blockType[1].width
+	blockType[1].y = scrHeight/2 - blockType[1].height;
+
+	blockType[2].x = scrWidth/6 + 120 -blockType[2].width;
+	blockType[2].y = scrHeight/2 - blockType[2].height;
+
+	blockType[3].x = scrWidth/6 + 180 -blockType[3].width;
+	blockType[3].y = scrHeight/2 - blockType[3].drawHeight;
 
 
 	//ground properties
 	ground = {
 		height: scrHeight /2,
 		width: 2,
-		color: 'rgb(0,0,0)',
+		fillColor: 'rgb(0,0,0)',
 	};
 
 	setInterval(timer,	1000/30); 
@@ -66,20 +72,30 @@ var charictar, ground,
 		canvas.style.backgroundColor = 'rgb(255,255,255)';
 
 		//Draw charictar
-		ctx.fillStyle = charictar.color;
+		ctx.fillStyle = charictar.fillColor;
 		ctx.fillRect(charictar.x,charictar.y,	charictar.width,charictar.height);
 
 		//Draw ground
-		ctx.fillStyle = ground.color;
+		ctx.fillStyle = ground.fillColor;
 		ctx.fillRect(0,ground.height,	scrWidth,ground.width);
 
 		//Draw normal block
-		ctx.fillStyle = blockType[0].color;
+		ctx.fillStyle = blockType[0].fillColor;
 		ctx.fillRect(blockType[0].x,blockType[0].y,    blockType[0].width,blockType[0].height);
 
 		//Draw bounce block
-		ctx.fillStyle = blockType[1].color;
+		ctx.fillStyle = blockType[1].fillColor;
 		ctx.fillRect(blockType[1].x,blockType[1].y,    blockType[1].width,blockType[1].height);
+
+		//Draw invisible block
+		ctx.strokeStyle = blockType[2].strokeColor;
+		ctx.lineWidth = blockType[2].strokeWidth;
+		ctx.rect(blockType[2].x,blockType[2].y,blockType[2].width,blockType[2].height);
+		ctx.stroke();
+
+		//Draw slime block
+		ctx.fillStyle = blockType[3].fillColor;
+		ctx.fillRect(blockType[3].x,blockType[3].y,    blockType[3].width,blockType[3].drawHeight);
 
 		//debug panel
 		ctx.fillStyle = 'rgb(0,0,0)';
@@ -87,6 +103,8 @@ var charictar, ground,
   		ctx.fillText(charictar.vY, scrWidth - 100, 10);
 		ctx.fillText(charictar.y,	scrWidth - 100, 20);
 		ctx.fillText("scrHeight: " + scrHeight/2,	scrWidth - 100, 30);
+		ctx.fillText(yBoundryActive[3],	scrWidth - 100, 40);
+		ctx.fillText(keyPress[38],	scrWidth - 100, 50);
 	}
 
 
@@ -105,19 +123,20 @@ var charictar, ground,
 		if (keyPress[38] && charictar.y == ground.height - charictar.height){
 			charictar.vY = -charictar.jumpHeight;
 			gameStart = true;	
-		} else if (keyPress[38] && yBoundryActive[0] == true){
+			}
+		for(var i = 0; i < 4; i++){
+			if (keyPress[38] && yBoundryActive[i] == true){
 			charictar.vY = -charictar.jumpHeight;
-		} else if (keyPress[38] && yBoundryActive[1] == true){
-			charictar.vY = -charictar.jumpHeight;
+			}
 		}
-
 	}
 
 
 	function boundryChecker(){
-
 		blockType[0].checkBoundry();
-		blockType[1].checkBoundry();		
+		blockType[1].checkBoundry();
+		blockType[2].checkBoundry();
+		blockType[3].checkBoundry();	
 	}
 
 
@@ -131,7 +150,8 @@ var charictar, ground,
 			charictar.vY += Gravity;
 		} 
 
-		if (charictar.y == ground.height -charictar.height){
+		if (charictar.y >= ground.height -charictar.height){
+			charictar.y = scrHeight/2 - charictar.height;
 			charictar.vY = 0;
 		} else if (charictar.vY == 0 && gameStart == true && charictar.y < (scrHeight/2 + charictar.y)){
 			charictar.vY += Gravity;
