5667374e99430b1196656ad5a97cf727c03176e6
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-12 19:11:19 +0000

    commit

--- InternalAngles/JS/index.js
+++ InternalAngles/JS/index.js
@@ -1,5 +1,19 @@
 var ctx = Canvas.getContext("2d"),
-	canvas = document.getElementById("Canvas");
+	canvas = document.getElementById("Canvas"),
+	n = 6,
+	sigmaTheta,
+	difX, dif;
 
-ctx.fillStyle = 'rgb(25,25,125)';
-ctx.fillRect(0,0,	50,10);
+sigmaTheta = n*(180-360/n); //∑θ=n(180-360/n)
+
+difX = Math.sin((sigmaTheta/n - 90)/180 * Math.PI) * 30;
+difY = Math.cos((sigmaTheta/n - 90)/180 * Math.PI) * 30;
+
+ctx.beginPath();
+ctx.strokeStyle = 'rgb(255,0,0)'
+ctx.moveTo(50, 50);
+ctx.lineTo(80, 50);
+ctx.lineTo(80 + difX, 50 + difY);
+ctx.lineTo(80, 50 + 2*difY);
+ctx.lineTo();
+ctx.stroke();
