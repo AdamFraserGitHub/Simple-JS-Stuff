c9e10a4d54321af4388a8d199d1b269105042321
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-14 19:58:21 +0000

    commit

--- Regular n Sided polygon/index.js
+++ Regular n Sided polygon/index.js
@@ -1,30 +1,41 @@
 var ctx = Canvas.getContext("2d"),
 	canvas = document.getElementById("Canvas"),
-    output = document.getElementById("textBox"),
-    numberOfSides = 2,
-    size = 100,
-    Xcenter = 1.25*size,
-    Ycenter = 1.25*size;
- 
-setInterval(draw,1000/3);
+    n = 10,
+    radius = 200,
+    centerX = 1.25*radius, centerX = 1.25*radius,
+    xArray = [], yArray = [];
 
-function draw(){
-    ctx.clearRect(0,0,800,800);
+var startPointX = Math.random() * (2*radius - radius) + radius,
+    startPointY = Math.random() * (2*radius - radius) + radius;
 
-    ctx.beginPath();
-    ctx.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));          
- 
-    for (var i = 1; i <= numberOfSides;i += 1) {
-       ctx.lineTo (Xcenter + size * Math.cos(i * 2 * Math.PI / numberOfSides), Ycenter + size * Math.sin(i * 2 * Math.PI / numberOfSides));
-    }
- 
-    ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 1;
-    ctx.stroke();
-    
-    // if (numberOfSides < 10){
-        numberOfSides+=1;
-    // }
+ctx.beginPath();
+ctx.strokeStyle = 'rgb(0,0,0)';
+ctx.lineWidth = 1;
 
-    output.innerHTML = numberOfSides - 1;
+ctx.moveTo (startPointX +  radius * Math.cos(0), startPointY +  radius *  Math.sin(0));          
+ 
+for (var i = 1; i <= n - 1; i++) {
+     centerX = Math.random() * (2*radius - radius) + radius;
+     centerY = Math.random() * (2*radius - radius) + radius;
+
+     xArray[i] = centerX + radius * Math.cos(i * 2 * Math.PI / n);
+     yArray[i] = centerY + radius * Math.sin(i * 2 * Math.PI / n);
+
+     ctx.lineTo (xArray[i], yArray[i]);
 }
+
+for (var i = 1; i <= n; i++){
+    ctx.moveTo(xArray[i],yArray[i]);
+    ctx.lineTo(xArray[i + 2],yArray[i + 2]);
+}
+
+ctx.stroke();
+ctx.closePath();
+
+
+ctx.beginPath();
+ctx.moveTo(xArray[n-1],yArray[n-1]);
+ctx.lineTo (startPointX + radius * Math.cos(n * 2 * Math.PI / n), startPointY + radius * Math.sin(n * 2 * Math.PI / n));
+ctx.strokeStyle = 'rgb(255,0,0)'
+ctx.stroke();
+ctx.closePath();
