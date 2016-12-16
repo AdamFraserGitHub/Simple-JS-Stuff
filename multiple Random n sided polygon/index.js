03c7c752d69029c0984033f0f230acde43ef868c
AdamFraserGitHub <AdamFraserPrograming@gmail.com>
2016-12-16 22:43:43 +0000

    commit

--- multiple Random n sided polygon/index.js
+++ multiple Random n sided polygon/index.js
@@ -4,7 +4,7 @@ var ctx = Canvas.getContext("2d"),
     radius = 50,
     centerX = [], centerY = [],
     xArray = [], yArray = [],
-    movt = 0;
+    movtY = 0, movtX = 0;
 
 setInterval(draw,1000/10)
 
@@ -19,7 +19,7 @@ ctx.beginPath();
 ctx.strokeStyle = 'rgb(0,0,0)';
 ctx.lineWidth = 1;
 
-ctx.moveTo (regularCenterX +  radius * Math.cos(0), regularCenterY +  radius *  Math.sin(0) + movt);          
+ctx.moveTo (regularCenterX +  radius * Math.cos(0) + movtX, regularCenterY +  radius *  Math.sin(0) + movtY);          
  
 for (var i = 1; i <= n - 1; i++) {
      centerX[i] = Math.random() * (2*radius - radius) + radius;
@@ -28,40 +28,40 @@ for (var i = 1; i <= n - 1; i++) {
      xArray[i] = centerX[i] + radius * Math.cos(i * 2 * Math.PI / n);
      yArray[i] = centerY[i] + radius * Math.sin(i * 2 * Math.PI / n);
 
-     ctx.lineTo (xArray[i], yArray[i] + movt);
+     ctx.lineTo (xArray[i] + movtX, yArray[i] + movtY);
 }
 
-ctx.moveTo(xArray[n-1],yArray[n-1] + movt);
-ctx.lineTo (regularCenterX + radius * Math.cos(n * 2 * Math.PI / n), regularCenterY + radius * Math.sin(n * 2 * Math.PI / n) + movt);
+ctx.moveTo(xArray[n-1] + movtX,yArray[n-1] + movtY);
+ctx.lineTo (regularCenterX + radius * Math.cos(n * 2 * Math.PI / n) + movtX, regularCenterY + radius * Math.sin(n * 2 * Math.PI / n) + movtY);
 
-ctx.moveTo(regularCenterX +  radius * Math.cos(0), regularCenterY +  radius *  Math.sin(0) + movt);
-ctx.lineTo(xArray[4],yArray[4] + movt);
+ctx.moveTo(regularCenterX +  radius * Math.cos(0) + movtX, regularCenterY +  radius *  Math.sin(0) + movtY);
+ctx.lineTo(xArray[4] + movtX,yArray[4] + movtY);
 
-ctx.moveTo(xArray[1],yArray[1] + movt);
-ctx.lineTo(xArray[3],yArray[3] + movt);
+ctx.moveTo(xArray[1] + movtX,yArray[1] + movtY);
+ctx.lineTo(xArray[3] + movtX,yArray[3] + movtY);
 
-ctx.moveTo(xArray[2],yArray[2] + movt);
-ctx.lineTo(xArray[5],yArray[5] + movt);
+ctx.moveTo(xArray[2] + movtX,yArray[2] + movtY);
+ctx.lineTo(xArray[5] + movtX,yArray[5] + movtY);
 
-ctx.moveTo(xArray[1],yArray[1] + movt);
-ctx.lineTo(xArray[4],yArray[4] + movt);
+ctx.moveTo(xArray[1] + movtX,yArray[1] + movtY);
+ctx.lineTo(xArray[4] + movtX,yArray[4] + movtY);
 
-ctx.moveTo(xArray[1],yArray[1] + movt);
-ctx.lineTo(xArray[5],yArray[5] + movt);
+ctx.moveTo(xArray[1] + movtX,yArray[1] + movtY);
+ctx.lineTo(xArray[5] + movtX,yArray[5] + movtY);
 
-ctx.moveTo(xArray[2],yArray[2] + movt);
-ctx.lineTo(regularCenterX +  radius * Math.cos(0), regularCenterY +  radius *  Math.sin(0) + movt);
+ctx.moveTo(xArray[2] + movtX,yArray[2] + movtY);
+ctx.lineTo(regularCenterX +  radius * Math.cos(0) + movtX, regularCenterY +  radius *  Math.sin(0) + movtY);
 
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
 
-ctx.arc(regularCenterX, regularCenterX + movt, 5, 0, Math.PI*2, false);
+ctx.arc(regularCenterX + movtX, regularCenterX + movtY, 5, 0, Math.PI*2, false);
 
 ctx.fillStyle = 'rgb(255,0,255)'
 ctx.fill();
 ctx.closePath()
 
-movt+=3*radius;
+movtY+=3*radius;
 }
